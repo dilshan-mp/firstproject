66497dd668607a7e1db860f5314d48ab0f79ba14
IT21185298 <it21185298@my.sliit.lk>
2024-09-20 23:29:03 +0530

    cards components done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Analytics from "./Components/Analytics";
 import Newsletter from "./Components/Newsletter";
+import Cards from "./Components/Cards";
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
       <Analytics />
-      <Newsletter/>
+      <Newsletter />
+      <Cards/>
     </div>
   );
 }
