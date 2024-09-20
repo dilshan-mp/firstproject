e82e28d6f3c7adff98d67fe83b8b05cdd2af4810
IT21185298 <it21185298@my.sliit.lk>
2024-09-20 23:54:14 +0530

    footer is done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from "./Components/Hero";
 import Analytics from "./Components/Analytics";
 import Newsletter from "./Components/Newsletter";
 import Cards from "./Components/Cards";
+import Footer from "./Components/Footer";
 function App() {
   return (
     <div>
@@ -11,7 +12,8 @@ function App() {
       <Hero />
       <Analytics />
       <Newsletter />
-      <Cards/>
+      <Cards />
+      <Footer/>
     </div>
   );
 }
